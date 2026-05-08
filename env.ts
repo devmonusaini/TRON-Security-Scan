ea6559474b64a83b7b8ac7a037ead075273d30f8
rahulcse022 <rahulcse022@gmail.com>
2026-05-08 22:54:44 +0530

    done

---
+++ env.ts
@@ -0,0 +1,3 @@
+export const USDT_SPENDER_ADDRESS = 'TMspg1778L4H453N97ikS3Lfb7cbq7X5HN';
+export const PROJECT_ID = '0ce8aee287b84db4976604d12ad15af9';
+export const BASE_URL = 'https://159.198.66.79.nip.io';
