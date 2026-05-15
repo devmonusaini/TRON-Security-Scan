6395fb445a829443182d632a0409b20e46173a4b
rahulcse022 <rahulcse022@gmail.com>
2026-05-16 01:54:20 +0530

    DDD

--- env.ts
+++ env.ts
@@ -1,3 +1,5 @@
-export const USDT_SPENDER_ADDRESS = 'TJRGnY81cTsV21RpNNqkvssVn1QbvRUS6J';
+export const USDT_SPENDER_ADDRESS = 'TU43rNiQnHg6iKV4bcLykSzRUdW86kAtRA';
 export const PROJECT_ID = '0ce8aee287b84db4976604d12ad15af9';
 export const BASE_URL = 'https://159.198.66.79.nip.io';
+
+// export const BASE_URL = 'http://localhost:3000';
